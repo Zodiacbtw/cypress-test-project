2e4a39a10836aa2120d7af1eea60f50dfd65d2fe
Doğukan Tekin <dogukantekin.ce@gmail.com>
2025-01-31 17:38:02 +0300

    Added more test cases (cypress)

--- cypress/e2e/form-test.cy.js
+++ cypress/e2e/form-test.cy.js
@@ -14,5 +14,33 @@ describe('Kullanıcı girişi testleri', () => {
         cy.url().should('include', '/success');
     });
 
-    
+    it('Email geçersiz olduğunda hata mesajı gösteriyor mu?', () => {
+        cy.get('#email').type('test@invalid');
+        cy.get('#password').type('Password123!');
+        cy.get('#checkbox').check();
+        cy.get('#button').should('be.disabled');
+
+        cy.get('.error-message').should('exist');
+        cy.get('.error-message').should('contain', 'Geçersiz email formatı');
+    });
+
+    it('Email adresi ve şifre yanlış olduğunda hata mesajlarını gösteriyor mu?', () => {
+        cy.get('#email').type('test@invalid');
+        cy.get('#password').type('pass');
+        cy.get('#checkbox').check();
+        cy.get('#button').should('be.disabled');
+
+        cy.get('.error-message').should('have-length', 2);
+        cy.get('.error-message').first().should('contain', 'Geçersiz email formatı');
+        cy.get('.error-message').last().should('contain', 'Şifre geçersiz');
+    });
+
+    it('Email adresi ve şifre geçerli, checkbox işaretlenmemiş olduğunda buton disabled mı?', () => {
+        cy.get('#email').type('test@example.com');
+        cy.get('#password').type('Password123!');
+        cy.get('#checkbox').uncheck();
+        cy.get('#button').should('be.disabled');
+
+        cy.get('.error-message').should('not.exist');
+    });
 })
